6fdc2fc2b247a95a4e22a2fcc648defe657136aa
Bilbo3340 <vladik.elce1@gmail.com>
2022-09-01 18:06:56 +0300

    upt

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (Integer i : sourceList) {
+            if (i % 2 == 0) {
+                list.add(i);
+            } else {
+                list.add(0, i);
+            }
+        }
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,26 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
+        for (int i = 0; i < sourceList.size() - 1; i++) {
+            if (Math.abs(Integer.parseInt(sourceList.get(i))) == Math.abs(Integer.parseInt(sourceList.get(i + 1)))
+            && Integer.parseInt(sourceList.get(i)) > Integer.parseInt(sourceList.get(i + 1))) {
+                String t = sourceList.get(i);
+                sourceList.set(i, sourceList.get(i + 1));
+                sourceList.set(i + 1, t);
+            }
+        }
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        return (int) (5 * Math.pow(Integer.parseInt(a), 2) - 3 - 5 * Math.pow(Integer.parseInt(b), 2) + 3);
     }
 }
